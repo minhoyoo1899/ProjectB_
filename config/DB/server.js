6a8a6804e8d880d43fb831d396d97207c133786a
MinhoYoo <minhoyoo1899@gmail.com>
2022-12-12 16:23:43 +0900

    디비용 http 서버 구동

--- config/DB/server.js
+++ config/DB/server.js
@@ -3,3 +3,19 @@ const http = require('http');
 const mysql = require('mysql');
 
 // console.log(http);
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    switch (req.url) {
+      case '/':
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end("인덱스");
+    }
+  } else if (req.url === "POST") {
+    console.log('this is POST')
+  }
+});
+
+server.listen(8282, () => {
+  console.log('server running 8282');
+});
