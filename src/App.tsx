dffad9932f256561582a2577d6ad6f08f138c05f
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-13 17:32:52 +0900

    클라이언트,서버 분리 + api 핸들링중

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import Map from "../src/Components/Map";
+
 const App = () => {
-  return <div>Hello Hojin</div>;
+  return (
+    // <div id="map">map</div>
+    <div>
+      <Map></Map>
+    </div>
+  )
 }
 export default App;
