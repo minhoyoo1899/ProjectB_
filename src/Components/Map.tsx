6e4bf10ee57c861e9c82baf10e31c300114e4153
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-14 13:07:18 +0900

    지도 두번 렌더링 되어서 해결중

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import axios from "axios"
 import "../CSS/map.css"
+import e from 'express'
 
 function Map() {
   //마커용
@@ -14,7 +15,7 @@ function Map() {
     latitude: "",
     longitude : ""
   })
-
+  
   //서버에 요청후 주소를 스테이트에 대입  
   useEffect(()=> {
     const getDatas = async()=>{
@@ -25,7 +26,7 @@ function Map() {
     }
     getDatas();
   },[])
-
+  
   // 현재위치의 위도값과 경도값을 받아서 state 저장 
   useEffect(()=> {
     // console.log(navigator.geolocation)
@@ -42,21 +43,67 @@ function Map() {
     }
   },[])
   
+  useEffect(()=>{
+    if(typeof location !== "string"){
+      const currentPosition = [location.latitude, location.longitude];
+
+      let trafficLayer = new naver.maps.TrafficLayer({
+        interval: 300000 // 5분마다 새로고침 (최소값 5분)
+      });
+    }
+  })
+
+  useEffect(()=> {
+    
+  })
+  
   // 현재 위치에 맞는 지도와 좌표에 마커 표시 
   useEffect(() => {
     if (typeof location !== "string") {
       const currentPosition = [location.latitude, location.longitude];
-
-      //지도관련 
+      
+      let trafficLayer = new naver.maps.TrafficLayer({
+          interval: 300000 // 5분마다 새로고침 (최소값 5분)
+        });
+      //지도생성
       mapRef.current = new naver.maps.Map("map", {
         center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
         zoom:18,
+        mapTypeControl: true,
         zoomControl: true,
       });
-      // const mapp = new naver.maps.Map('map',mapOptions)
-      // const mapClick = naver.maps.Event.addListener(map,"click",(e)=>{
-      //   console.log(e)
-      // }))
+      console.log(mapRef.current)
+      trafficLayer.setMap(mapRef.current)
+        
+        
+      // naver.maps.Event.once(markRef.current,"init",(e)=>{
+      //   trafficLayer.setMap(mapRef.current)
+      // })
+        //클릭한 위치에 마커 생성
+      naver.maps.Event.addListener(mapRef.current,"click",(e)=> {
+        // console.log(e)
+        let infoWindow = new naver.maps.InfoWindow({
+          content: [
+            '<div class="iw_inner">',
+            `<h2>선택한 좌표값</h2>`,
+            `<p>${e.coord._lat}<br>
+            ${e.coord._lng}`,
+            '</p>',
+            '</div>'
+          ].join('')
+        });
+        let newMarker = new naver.maps.Marker({
+          position:e.coord,
+          map:mapRef.current
+        })
+        if(infoWindow.getMap()) {
+          infoWindow.close()
+        } else {
+          infoWindow.open(mapRef.current,newMarker)
+          console.log(infoWindow)
+        }
+      })
+      // infoWindow.open()
       //마커관련
       markRef.current = new naver.maps.Marker({
         position : new naver.maps.LatLng(currentPosition[0],currentPosition[1]),
@@ -64,13 +111,27 @@ function Map() {
         //아이콘 커스텀
         icon: {
           url: "https://mblogthumb-phinf.pstatic.net/MjAyMTA4MDRfMjk0/MDAxNjI4MDg2MDEwNTY4.TTh8QJAkzwBZZpKSw8OpIK83JQ8dBBI5qBu9uWvgrKUg.X2keaCQ5fJkyx05OOlVYwKPi3ynq0_oqHv-UFqLyGsQg.PNG.cha-cha97/%EC%B6%98%EC%8B%9D%EC%9D%B4%EF%BC%BF001.png?type=w800", 
-          // url : "chunsik.png",
           scaledSize : new naver.maps.Size(80,80),
         }
+      })
+      //마커 마우스 호버
+      naver.maps.Event.addListener(markRef.current,"mouseover"
+      ,(e)=>{
+        console.log(e)
+        console.log("마우스올림")
+      })
+      //마커 마우스 탈출 
+      naver.maps.Event.addListener(markRef.current,"mouseout"
+      ,(e)=>{
+        // console.log(e)
+        console.log("마우스빠짐")
       })
       console.log(markRef.current)
     }
   }, [mapRef,location]);
+
+
+
     return (
       <div id='rootBg'>
       <p>현재위치</p>
