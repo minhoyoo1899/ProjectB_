ee5c48dec1756e3eedecd78a6693571a66837e42
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-15 12:08:30 +0900

    중간 푸시

--- src/Components/Map copy.tsx
+++ src/Components/Map copy.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import axios from "axios"
 import "../CSS/map.css"
+import e from 'express'
 
 function Map() {
   //마커용
@@ -14,91 +15,52 @@ function Map() {
     latitude: "",
     longitude : ""
   })
-  //경로 그리기용 state
-  const [routePathArr, setRoutePathArr] = useState<any>([])
-  // console.log(routePathArr)
   
   //서버에 요청후 주소를 스테이트에 대입  
   useEffect(()=> {
     const getDatas = async()=>{
       const datas = await axios.get("http://localhost:6565/")
-      const routePath = datas.data.route.traoptimal[0].path
-      // console.log(routePath)
-      setRoutePathArr(routePath)
-      // console.log(datas.data.addresses[0].roadAddress)
-      // let myhome = datas.data.addresses[0].roadAddress
-      // setHome(myhome)
+      console.log(datas.data.addresses[0].roadAddress)
+      let myhome = datas.data.addresses[0].roadAddress
+      setHome(myhome)
     }
     getDatas();
   },[])
   
   // 현재위치의 위도값과 경도값을 받아서 state 저장 
   useEffect(()=> {
+    // console.log(navigator.geolocation)
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
+        // console.log(position)
         setLocation({
-          latitude: Number(position.coords.latitude),
-          longitude: Number(position.coords.longitude),
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
         });
       });
     } else {
       window.alert("현재위치를 알수 없습니다.");
     }
   },[])
-  console.log(location)
 
-  //경로 그리기용 path 담을 배열 
-  let polylinePath:Number[] = []
-  routePathArr.map((data:any)=> {
-    // console.log(data)
-    polylinePath.push(data)
-  })
-  // console.log(polylinePath)
+  useEffect(()=> {
+    // if(typeof location !== "string"){
+      const currentPosition = [location.latitude, location.longitude];
+      
+      let trafficLayer = new naver.maps.TrafficLayer({
+          interval: 300000 // 5분마다 새로고침 (최소값 5분)
+        });
 
-  // 현재 위도 경도 값 요청 
-  useEffect(()=>{
-      // let trafficLayer = new naver.maps.TrafficLayer({
-      //   interval: 300000 // 5분마다 새로고침 (최소값 5분)
-      // });
-      console.log(location)
-
+      //지도생성
       mapRef.current = new naver.maps.Map("map", {
-        center: new naver.maps.LatLng(location.latitude, location.longitude),
-        // center: new naver.maps.LatLng(36.343457, 127.392416),
+        center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
         zoom:18,
         mapTypeControl: true,
         zoomControl: true,  
       });
       console.log(mapRef.current)
-      // trafficLayer.setMap(mapRef.current)
-  
-      // naver.maps.Event.once(mapRef.current,"init",(e)=>{
-      //     trafficLayer.setMap(mapRef.current)
-      //   })
-  },[location])
-
-  // 지도
-  useEffect(()=>{
-    let trafficLayer = new naver.maps.TrafficLayer({
-      interval: 300000 // 5분마다 새로고침 (최소값 5분)
-    });
-    mapRef.current = new naver.maps.Map("map", {
-      center: new naver.maps.LatLng(location.latitude, location.longitude),
-      zoom:18,
-      mapTypeControl: true,
-      zoomControl: true,  
-    });
-    console.log(mapRef.current)
-    trafficLayer.setMap(mapRef.current)
-    naver.maps.Event.once(mapRef.current,"init",(e)=>{
       trafficLayer.setMap(mapRef.current)
-    })
-  },[mapRef])
-  //현재위치 마커 
-  useEffect(()=> {
-    // if(typeof location !== "string"){
-      const currentPosition = [location.latitude, location.longitude];
+
       //마커관련
       markRef.current = new naver.maps.Marker({
         position : new naver.maps.LatLng(currentPosition[0],currentPosition[1]),
@@ -124,8 +86,28 @@ function Map() {
     // }
   },[location])
   
+  // 현재 위치에 맞는 지도와 좌표에 마커 표시 
   useEffect(() => {
     if (typeof location !== "string") {
+      // const currentPosition = [location.latitude, location.longitude];
+      
+      // let trafficLayer = new naver.maps.TrafficLayer({
+      //     interval: 300000 // 5분마다 새로고침 (최소값 5분)
+      //   });
+      // //지도생성
+      // mapRef.current = new naver.maps.Map("map", {
+      //   center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
+      //   zoom:18,
+      //   mapTypeControl: true,
+      //   zoomControl: true,
+      // });
+      // console.log(mapRef.current)
+      // trafficLayer.setMap(mapRef.current)
+        
+        
+      // naver.maps.Event.once(markRef.current,"init",(e)=>{
+      //   trafficLayer.setMap(mapRef.current)
+      // })
         //클릭한 위치에 마커 생성
       naver.maps.Event.addListener(mapRef.current,"click",(e)=> {
         // console.log(e)
@@ -150,8 +132,35 @@ function Map() {
           console.log(infoWindow)
         }
       })
+      // infoWindow.open()
+      // //마커관련
+      // markRef.current = new naver.maps.Marker({
+      //   position : new naver.maps.LatLng(currentPosition[0],currentPosition[1]),
+      //   map : mapRef.current,
+      //   //아이콘 커스텀
+      //   icon: {
+      //     url: "https://mblogthumb-phinf.pstatic.net/MjAyMTA4MDRfMjk0/MDAxNjI4MDg2MDEwNTY4.TTh8QJAkzwBZZpKSw8OpIK83JQ8dBBI5qBu9uWvgrKUg.X2keaCQ5fJkyx05OOlVYwKPi3ynq0_oqHv-UFqLyGsQg.PNG.cha-cha97/%EC%B6%98%EC%8B%9D%EC%9D%B4%EF%BC%BF001.png?type=w800", 
+      //     scaledSize : new naver.maps.Size(80,80),
+      //   }
+      // })
+      // //마커 마우스 호버
+      // naver.maps.Event.addListener(markRef.current,"mouseover"
+      // ,(e)=>{
+      //   console.log(e)
+      //   console.log("마우스올림")
+      // })
+      // //마커 마우스 탈출 
+      // naver.maps.Event.addListener(markRef.current,"mouseout"
+      // ,(e)=>{
+      //   // console.log(e)
+      //   console.log("마우스빠짐")
+      // })
+      // console.log(markRef.current)
     }
-  }, [location]);
+  }, [mapRef,location]);
+
+
+
     return (
       <div id='rootBg'>
       <p>현재위치</p>
--- src/Server/server.js
+++ src/Server/server.js
@@ -23,7 +23,8 @@ app.get("/", async(req,res)=> {
   try {
     let result = await axios({
       method : "get",
-      url : `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${address}`,
+      // url : `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${address}`,
+      url: "https://naveropenapi.apigw.ntruss.com/map-direction-15/v1/driving?start=127.392416,36.343457&goal=127.377575,36.34925&cartype=4",
       headers: header
     });
 
