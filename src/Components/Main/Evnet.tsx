7f534d5a518eac96181f716511a8ba6e5f3bdd57
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-12-19 18:25:35 +0900

    돌발데이터 조건부 적용(사고 정보, 돌발정보 분리)

--- src/Components/Main/Evnet.tsx
+++ src/Components/Main/Evnet.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 const Event = () =>{
   const [eventView,setEventView] = useState([])
     useEffect(()=>{
-      fetch(`https://openapi.its.go.kr:9443/eventInfo?apiKey=${process.env.REACT_APP_ItsKey}&type=all&eventType=all&minX=126.800000&maxX=127.890000&minY=34.900000 &maxY=35.100000&getType=json`)
+      fetch(`https://openapi.its.go.kr:9443/eventInfo?apiKey=${process.env.REACT_APP_ItsKey}&type=all&eventType=all&minX=127.234227&maxX=127.570949&minY=36.192958 &maxY=36.488949&getType=json`)
         .then((response)=>response.json())
         .then((response)=>{
           console.log(response.body.items)
@@ -19,7 +19,28 @@ const Event = () =>{
     <Container>
       <div>
         <Title color="tomato">사고정보</Title>
-        <Content>00 구간 화물차 사고</Content>
+        {
+          eventView.map((item:any)=>{
+            return(
+              
+              <Content key={item.linkId}>
+                
+                {item.eventType === "교통사고" ? (
+                  // 이벤트 메시지가 존재하면 메시지 띄우고 없으면 이벤트타입 데이터 띄우도록
+                  <>
+                    <div>{item.roadName}</div>
+                    {item.message !== "" ? (
+                      <div>{item.message}</div>
+                    ):(<div>{item.eventDetailType}</div>)}
+                    
+                  </>
+                ):(<div>데이터가 없습니다</div>)}
+
+              </Content>
+            )
+          })
+        }
+        
       </div>
       {/* 돌발정보 데이터 반복 삽입 */}
       <div>
@@ -28,8 +49,14 @@ const Event = () =>{
           eventView.map((item:any)=>{
             return(
               <Content key={item.linkId}>
-                <div style={{fontWeight:'bold'}}>▪{item.roadName}</div>
-                {item.message !== "" ? (<div>{item.message}</div>):(item.eventDetailType)}
+                {item.eventType !== "교통사고" ? (
+                  <>
+                    <div>{item.roadName}</div>
+                    {item.message !== "" ? (
+                      <div>{item.message}</div>
+                    ):(<div>{item.eventDetailType}</div>)}
+                  </>
+                ):(null)}
               </Content>
             )
             
@@ -69,3 +96,4 @@ const Content = styled.div`
   gap:5px;
   
 `
+
