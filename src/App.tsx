fbc867c637290af1062304f4985cced7bc3ce249
MinhoYoo <minhoyoo1899@gmail.com>
2022-12-19 13:50:52 +0900

    map 불러오는 경로 수정 (app < map ====>>>> app < main < map)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Minho from "./Components/Minho";
 import Map from "./Components/Map/Map"
+import Main from "./Components/Main/Main"
 const App = () => {
   return (
     <>      
       <div>
-        <Map />
+        <Main />
       </div>
     </>
   );
