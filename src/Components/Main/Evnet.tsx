afc7d38529b04fa2efad7f2cbf903e030241c4a3
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-12-19 17:44:37 +0900

    돌발 데이터 적용

--- src/Components/Main/Evnet.tsx
+++ src/Components/Main/Evnet.tsx
@@ -1,18 +1,41 @@
 
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const Event = () =>{
+  const [eventView,setEventView] = useState([])
+    useEffect(()=>{
+      fetch(`https://openapi.its.go.kr:9443/eventInfo?apiKey=${process.env.REACT_APP_ItsKey}&type=all&eventType=all&minX=126.800000&maxX=127.890000&minY=34.900000 &maxY=35.100000&getType=json`)
+        .then((response)=>response.json())
+        .then((response)=>{
+          console.log(response.body.items)
+          setEventView(response.body.items)
+        }).catch((err)=>{
+          console.log(err)
+        })
+    },[])
+
   return(
     <Container>
       <div>
         <Title color="tomato">사고정보</Title>
-        <Content>00 구간 정체중</Content>
-      </div>
-      <div>
-        <Title color="#FFC314">돌발정보</Title>
         <Content>00 구간 화물차 사고</Content>
       </div>
-      
+      {/* 돌발정보 데이터 반복 삽입 */}
+      <div>
+        <Title color="#FFC314">돌발정보</Title>
+        {
+          eventView.map((item:any)=>{
+            return(
+              <Content key={item.linkId}>
+                <div style={{fontWeight:'bold'}}>▪{item.roadName}</div>
+                {item.message !== "" ? (<div>{item.message}</div>):(item.eventDetailType)}
+              </Content>
+            )
+            
+          })
+        }
+      </div>
     </Container>
   )
 }
@@ -26,7 +49,6 @@ const Container = styled.div`
   flex-direction:column;
   gap:50px;
 `
-
 //타이틀
 const Title = styled.div`
   width:100%;
@@ -39,7 +61,11 @@ const Title = styled.div`
 const Content = styled.div`
   width:100%;
   height:max-content;
-  padding:5%;
+  padding:3%;
   background-color:rgba(40,40,40,0.5);
   color:white;
+  display:flex;
+  flex-direction:column;
+  gap:5px;
+  
 `
